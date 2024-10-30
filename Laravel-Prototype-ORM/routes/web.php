db08f7d56c77a0dbc3812c051a14b570e5af42e6
Dev Zakaria <Azizi.zakaria.solicode@gmail.com>
2024-10-30 15:27:53 +0100

    Update

--- Laravel-Prototype-ORM/routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\PostController;
-
-Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
-Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
