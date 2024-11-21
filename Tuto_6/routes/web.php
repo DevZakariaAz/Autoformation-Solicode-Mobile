4a375396210fb75a09cf9919e14b5388ec1caa32
Dev Zakaria <Azizi.zakaria.solicode@gmail.com>
2024-11-21 03:04:34 +0100

    Tuto 6

---
+++ Tuto_6/routes/web.php
@@ -0,0 +1,5 @@
+<?php
+use App\Http\Controllers\ArticleController;
+
+Route::get('/articles', [ArticleController::class, 'index'])->name('articles.index');
+Route::get('/articles/{id}', [ArticleController::class, 'show'])->name('articles.show');
